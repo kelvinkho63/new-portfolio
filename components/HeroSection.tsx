deae2292c82aeb299b7bd8f115dd455433c5d36c
kelvinkho63 <kelvinkho64@gmail.com>
2024-10-28 04:50:53 +0700

    first commit

---
+++ components/HeroSection.tsx
@@ -0,0 +1,39 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { Link } from "react-scroll/modules"
+
+const HeroSection = () => {
+  return (
+    <section id="home">
+        <div className='flex flex-col text-center items-center justify-center'>
+            <div>
+                <Image className="rounded-full shadow-2xl" src="/photo.jpg" alt="" width={300} height={300}/>
+            </div>
+            <div>
+                <h1 className="font-bold text-4xl mt-6">Hi, i'm Kelvin</h1>
+                <p className="font-semibold text-blue-600">
+                    I'm a {" "}
+                    <span className="font-semibold text-blue-600">
+                        Software Engineer {" "}
+                    </span> 
+                    currently based in Jakarta. Striving to be better.
+                </p>
+                <Link 
+                    to="projects" 
+                    smooth={true} 
+                    className="text-xl font-semibold border-b-2 border-blue-600"
+                    activeClass='active'
+                    spy={true}
+                    offset={-100}
+                    duration={500}
+                    >
+                        Projects
+                    </Link>
+            </div>
+        </div>
+        </section>
+  )
+}
+
+export default HeroSection
